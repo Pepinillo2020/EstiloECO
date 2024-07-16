06a5f21bf605511ef33a3e5d6de90361820382bd
IgnacioPuelles <139293045+IgnacioPuelles@users.noreply.github.com>
2024-07-16 18:29:56 -0400

    Estilo horario, contacto

--- estiloeco/main/forms.py
+++ estiloeco/main/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Producto
+from .models import Producto, Contacto
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -13,3 +15,14 @@ class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
         fields = ['nombre', 'categoria', 'descripcion', 'precio', 'imagen']
+
+class ContactoForm(forms.ModelForm):
+    class Meta:
+        model = Contacto
+        fields = ['nombre', 'email', 'asunto', 'mensaje']
+
+    def __init__(self, *args, **kwargs):
+        super(ContactoForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Enviar', css_class='btn btn-primary container'))
