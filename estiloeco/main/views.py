e867a55be8ba963816d8f2768047c5f220f66e27
vi.cornou <133885035+ElCornuwu@users.noreply.github.com>
2024-07-16 21:39:02 -0400

    arreglo

--- estiloeco/main/views.py
+++ estiloeco/main/views.py
@@ -25,7 +25,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('index')
+            return redirect('home')
     else:
         form = CustomUserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
